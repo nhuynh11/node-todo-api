b261670016beb247af6f5e83815a211efb166506
nhuynh11 <nphuynh11@gmail.com>
2017-11-03 17:32:27 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59fb9960f2676b81f2bc226c')
+  // }, {
+  //   // using update operators
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59fb671a92ff643c2cb690ee')
+  }, {
+    // using update operators
+    $set: {
+      name: 'Nick'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
